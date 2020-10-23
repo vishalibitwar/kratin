8f17f6649b30a645269061bfceb6624e4186ba9e
vishal ibitwar <vish31ibitwar@gmail.com>
2020-10-23 23:50:04 +0530

    Updates

---
+++ precache-manifest.72000ce98fcb3158473a8ec940a0e1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d9608bc978d100533a09f5d5f967ea",
+    "url": "/kratin/index.html"
+  },
+  {
+    "revision": "0a6942b03554aff8915d",
+    "url": "/kratin/static/css/main.28d48a8d.chunk.css"
+  },
+  {
+    "revision": "d5d4ae438c63e88177ee",
+    "url": "/kratin/static/js/2.4e5ea661.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/kratin/static/js/2.4e5ea661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6942b03554aff8915d",
+    "url": "/kratin/static/js/main.3cd68568.chunk.js"
+  },
+  {
+    "revision": "3650fdc396ca47447099",
+    "url": "/kratin/static/js/runtime-main.9321a371.js"
+  }
+]);
